5edafc95387deed227edcc81f3d059c141d21ac0
Reiiiii <54180419+wuzizizizi-xiao@users.noreply.github.com>
2026-09-01 22:04:15 +0800

    Delete packages directory

--- packages/shared/src/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export { formatDistance, formatDuration, maskPhone, maskPlate, formatTime, estimateCalories, estimateCarbonSaved, treeEquivalent, calcDistance } from './format';
-export { normalizeRedemptionStatus, REDEMPTION_STATUS_META, isValidDateValue, formatDateSafe, formatExpiryDate, resolveRedemptionStatus } from './redemption';
